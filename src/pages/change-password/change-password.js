58d516253bcbd6ff54b1b123e549d18cbe9ecd95
Aleksandr Panichenko <nasada@yandex.ru>
2021-06-07 13:08:37 +0300

    удаление забытой логики сбора данных с формы

--- src/pages/change-password/change-password.js
+++ src/pages/change-password/change-password.js
@@ -8,15 +8,3 @@ import "./change-password.scss";
 
 const app = document.getElementById("app");
 app.innerHTML = compiledTemplate(data);
-
-const editInfoForm = document.getElementById("editInfoForm");
-editInfoForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const data = new FormData(event.target);
-  console.log({
-    oldPassword: data.get("oldPassword"),
-    password: data.get("password"),
-    confirmPassword: data.get("confirmPassword"),
-  });
-  window.location = "/profile.html";
-});
